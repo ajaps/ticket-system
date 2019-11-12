e738b1a043932304f015c4f04e5ca960a34a665e
ajaps <ajapsfranklin@gmail.com>
2019-11-12 08:18:53 +0100

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User
+User.create(name: 'JohnDoe@admin.com', admin: true, agent: false, email: 'JohnDoe@admin.com', password: 'password')
